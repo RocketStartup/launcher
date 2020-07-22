36bfc6daabc5a9b086ac43c4b014f7bd6a7aac20
Henrique Machiavelli <hsmachiavelli@gmail.com>
2020-07-22 20:41:58 -0300

    including DIRECTORY_SEPARATOR

--- src/Connectors/Orm.php
+++ src/Connectors/Orm.php
@@ -2,38 +2,47 @@
 
 namespace Astronphp\Handler\Connectors;
 
-class Orm{
+class Orm
+{
     public  $return;
-    private $parms      = array();
-    private $commands   = array(
-                                "orm:create-entity"         =>      'php '.PATH_ROOT.'vendor/astronphp/orm/src/schema/generate.php',
-                                "orm:update-db"             =>      PATH_ROOT.'vendor/bin/doctrine orm:schema-tool:update --force'
-                          );
+    private $params   = [];
+    private $commands = [
+                            "orm:create-entity"         =>      'php '.PATH_ROOT.'vendor/astronphp/orm/src/schema/generate.php',
+                            "orm:update-db"             =>      PATH_ROOT.'vendor/bin/doctrine orm:schema-tool:update --force'
+                        ];
     
-    public function __construct($p=array()){
-        $this->parms = $p;
+    public function __construct($p = [])
+    {
+        $this->params = $p;
         $this->moveConfigRoot();
         $this->executeShell();
         $this->removeConfigRoot();
         return $this;
     }
 
-    private function moveConfigRoot(){
-        copy(PATH_ROOT.'vendor/astronphp/orm/src/schema/cli-config.php', PATH_ROOT.'cli-config.php');
+    private function moveConfigRoot()
+    {
+        $sep = DIRECTORY_SEPARATOR;
+
+        copy(PATH_ROOT.'vendor' . $sep . 'astronphp' . $sep . 'orm' . $sep . 'src' . $sep . 'schema' . $sep . 'cli-config.php', PATH_ROOT.'cli-config.php');
     }
 
-    private function executeShell(){
-        if(in_array($this->parms[1],array_flip($this->commands))==true){
-            $this->return = system($this->commands[$this->parms[1]]);
-        }else{
-            array_shift($this->parms);
-            $this->return = system('./vendor/bin/doctrine '.implode(' ',$this->parms));
+    private function executeShell()
+    {
+        if (in_array($this->params[1],array_flip($this->commands))==true) {
+            $this->return = system($this->commands[$this->params[1]]);
+        } else {
+            $sep = DIRECTORY_SEPARATOR;
+
+            array_shift($this->params);
+            $this->return = system('.'. $sep . 'vendor'. $sep . 'bin'. $sep . 'doctrine ' . implode(' ',$this->params));
         }
     }
 
-    private function removeConfigRoot(){
-        if(file_exists(PATH_ROOT."cli-config.php")) {
-            unlink(PATH_ROOT."cli-config.php"); 
+    private function removeConfigRoot()
+    {
+        if (file_exists(PATH_ROOT . "cli-config.php")) {
+            unlink(PATH_ROOT . "cli-config.php"); 
         }
     }
 }
